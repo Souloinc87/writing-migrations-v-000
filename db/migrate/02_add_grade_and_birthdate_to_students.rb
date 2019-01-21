847f898a57d07a8fa24074872ff41884e942b66c
Marcus  = Soulo <souloinc87@gmail.com>
2019-01-21 08:03:18 +0000

    Automatically backed up by Learn

--- db/migrate/02_add_grade_and_birthdate_to_students.rb
+++ db/migrate/02_add_grade_and_birthdate_to_students.rb
@@ -1,8 +1,7 @@
 class AddGradeAndBirthdateToStudents < ActiveRecord::Migration[5.1]
   def change
-    add_column :students do |t|
-      t.integer :grade
-      t.string :birthdate
+    add_column :students, :grade, :string 
+    add_column :students, :birthdate, :integer 
     end
   end
 end
